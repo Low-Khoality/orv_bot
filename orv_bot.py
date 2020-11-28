b1cf4ee46782fff6474f12b2c60e6492e43faecd
Low Khoality <someepicgamer22@gmail.com>
2020-11-28 17:04:15 -0500

    update

---
+++ orv_bot.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+from discord.ext import commands
+from settings import TOKEN
+
+BOT_PREFIX = ("?", "!")
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+
+
+async def list_servers():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        print("\nCurrent servers:")
+        for server in bot.guilds:
+            print(server.name+'\n')
+        await asyncio.sleep(600)
+
+
+bot.loop.create_task(list_servers())
+bot.run(TOKEN)
