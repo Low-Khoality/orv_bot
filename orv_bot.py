13737d871c589a7ee271354ac61c8fe78f7825d2
Low Khoality <someepicgamer22@gmail.com>
2020-11-29 08:04:06 -0500

    added status

--- orv_bot.py
+++ orv_bot.py
@@ -6,6 +6,11 @@ from settings import TOKEN
 BOT_PREFIX = ("?", "!")
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
+@bot.event
+async def on_ready():
+    print('Bot is now working!')
+    await bot.change_presence(activity=discord.Streaming(name="the scenarios", url="https://www.twitch.tv/someepicgamer22"))
+
 async def list_servers():
     await bot.wait_until_ready()
     while not bot.is_closed():
@@ -13,6 +18,7 @@ async def list_servers():
         for server in bot.guilds:
             print(server.name+'\n')
         await asyncio.sleep(600)
+
 
 
 bot.loop.create_task(list_servers())
