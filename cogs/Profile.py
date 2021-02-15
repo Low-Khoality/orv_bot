041b0a3214cb396d5e4cebed14a4e545e65f3e95
Low Khoality <someepicgamer22@gmail.com>
2021-02-15 10:32:28 -0500

    Update Profile.py
    now when you profile another member it shows their stats instead of yours

--- cogs/Profile.py
+++ cogs/Profile.py
@@ -14,11 +14,12 @@ class Profile(commands.Cog):
                       enabled=True,
                       brief="Shows how many coins you currently have",
                       usage=["[Example] oc"])
-    async def balance(self, ctx):
-        coins = self.get.get_coins(ctx.author.id)
+    async def balance(self, ctx, member: discord.Member = None):
+        member = ctx.author if not member else member
+        coins = self.get.get_coins(member.id)
         if coins is not None:
             embed = discord.Embed(description=f"**Coins possessed:** {format(coins, ',d')}", color=discord.Color.from_rgb(130, 234, 255))
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+            embed.set_author(name=member.name, icon_url=member.avatar_url)
             # embed = discord.Embed(title=f"[{get_user_type(ctx.author.id)} **{ctx.author.name}** you have obtained __{coins}__ coins!]", color=discord.Color.from_rgb(130, 234, 255))
             await ctx.send(embed=embed)
         else:
@@ -47,21 +48,23 @@ class Profile(commands.Cog):
                       usage=["[Example] `os` -> Shows your stats", "[Example] `os @user` -> Shows another users profile"])
     async def stats(self, ctx, member: discord.Member = None):
         member = ctx.author if not member else member
-        if self.get.get_user(ctx.author.id):
-            evaluation = self.get.get_evaluation(ctx.author.id)
+        if self.get.get_user(member.id):
+            evaluation = self.get.get_evaluation(member.id)
             if evaluation:
                 addition = f"**Overall Evaluation:** \n{evaluation}\n\n"
             else:
                 addition = "**Overall Evaluation** \nThe current comprehensive evaluation is \ncurrently in progress.\n\n"
-            level = self.get.get_level(ctx.author.id)
+            level = self.get.get_level(member.id)
             block1=f"{addition} **Name:** {member.name}\n**Level:** {level}"
 
-            user_type = self.get.get_user_type(ctx.author.id)
+            user_type = self.get.get_user_type(member.id)
             if user_type == "Constellation":
+                # TODO add constellation modifiers to database
                 block1 += f"\n**Constellation Modifier:** *Architect of revelation*"  # placeholder value
             elif user_type == "Incarnation":
+                # TODO code here
                 block1 += f"\n**Constellation sponsor:** N/A"  # placeholder value
-            nebula = self.get.get_nebula(ctx.author.id)
+            nebula = self.get.get_nebula(member.id)
             if nebula:
                 block1 += f"\n**Nebula Backing:** {nebula}"
 
@@ -73,14 +76,14 @@ class Profile(commands.Cog):
 
             embed.timestamp = datetime.now()
 
-            embed.add_field(name="\u200b", value=f"{block1}\n**Coins:** {self.get.get_coins(ctx.author.id)}", inline=True)
+            embed.add_field(name="\u200b", value=f"{block1}\n**Coins:** {self.get.get_coins(member.id)}", inline=True)
 
-            attribute = self.get.get_attributes(ctx.author.id)
-            personal_skills = self.get.get_skills(ctx.author.id)
+            attribute = self.get.get_attributes(member.id)
+            personal_skills = self.get.get_skills(member.id)
             embed.add_field(name=f"**Personal Attribute(s):** ", value=f"{attribute[:-2]}", inline=False)
             embed.add_field(name=f'**Personal Skill(s):** ', value=f'{personal_skills[:-2]}', inline=False)
 
-            general_skills = self.get.get_general_skills(ctx.author.id)
+            general_skills = self.get.get_general_skills(member.id)
             embed.add_field(name=f'**Overall Stats:** ', value=f"[Stamina Lv.{general_skills[0]}], [Strength Lv.{general_skills[1]}],\n[Agility Lv.{general_skills[2]}], [Magic Power Lv.{general_skills[3]}]", inline=False)
 
             await ctx.send(embed=embed)
