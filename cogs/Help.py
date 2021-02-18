6fe55441f5b3be7a09823bb11d0cb1cc294dcb1c
Low Khoality <someepicgamer22@gmail.com>
2021-02-18 16:21:02 -0500

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -4,6 +4,11 @@ from discord.ext import commands
 async def has_permissions(ctx):
     return ctx.message.author.guild_permissions.administrator
 
+def is_khoa(ctx):
+    return ctx.author.id == 122837007834677251
+
+def is_admin(ctx):
+    return ctx.author.id in [423674541886406656, 122837007834677251, 526152115140427782]
 
 class Help (commands.Cog):
     def __init__(self, bot):
@@ -14,29 +19,28 @@ class Help (commands.Cog):
     async def get_help(self, ctx, command: str):
         cmd = self.bot.get_command(command)
 
-        if cmd.hidden==True:
+        if cmd is None:
+            return await self.error.get_error(ctx, f'No command called "{command}" found', "help")
+
+        if cmd.hidden==True and not is_admin(ctx):
             return await self.error.get_error(ctx, f'No command called "{command}" found', "help")
 
         aliases = ""
-        try:
-            brief = ". . ."
-            if cmd.brief:
-                brief = cmd.brief
+        brief = ". . ."
+        if cmd.brief:
+            brief = cmd.brief
 
-            title = f"Command: {cmd}"
-            if cmd.aliases:
-                for i in cmd.aliases:
-                    aliases += f"{i}, "
-                title += f' [Other names: {aliases[:-2]}]'
+        title = f"Command: {cmd}"
+        if cmd.aliases:
+            for i in cmd.aliases:
+                aliases += f"{i}, "
+            title += f' [Other names: {aliases[:-2]}]'
 
-            usage = ""
-            if cmd.usage:
-                for i in cmd.usage:
-                    usage += f"\n{i}"
-        except:
-            if cmd is None:
-                await self.error.get_error(ctx, f'No command called "{command}" found', "help")
-                return
+        usage = ""
+        if cmd.usage:
+            for i in cmd.usage:
+                usage += f"\n{i}"
+
         embed = discord.Embed(title=title, description=f"{brief}{usage}", color=discord.Color.from_rgb(130, 234, 255))
         embed.set_footer(text=f"Use {self.get.get_prefix(ctx)}help <command or command group name> for more info on a command")
         try:
@@ -44,7 +48,7 @@ class Help (commands.Cog):
                 brief = ". . ."
                 if command.brief:
                     brief = command.brief
-                embed.add_field(name=command, value=brief, inline=False)
+                embed.add_field(name=f"{command} {command.description}", value=brief, inline=False)
         except:
             pass
         await ctx.send(embed=embed)
@@ -62,7 +66,8 @@ class Help (commands.Cog):
             cog_list = ["Profile",
                         "Settings",
                         "Misc",
-                        "Nebulas"]
+                        "Nebulas",
+                        "Admin"]
 
             for cog in cog_list:
                 cog_names.append(cog)
@@ -72,7 +77,7 @@ class Help (commands.Cog):
             for i in range(len(commands)):
                 temp = ""
                 for c in commands[i]:
-                    if c.hidden==False:
+                    if c.hidden==False or is_admin(ctx):
                         temp += f"{c.name} | "
                 command_names.append(temp)
 
@@ -80,7 +85,9 @@ class Help (commands.Cog):
 
             # if owner is true then show the Settings cog (i == 2) else dont show it
             for i in range(len(cogs)):
-                if owner is True and i == 1:
+                if owner and i == 1:
+                    embed1.add_field(name=cog_names[i], value=(command_names[i])[:-2], inline=True)
+                if is_admin(ctx) and i == 4:
                     embed1.add_field(name=cog_names[i], value=(command_names[i])[:-2], inline=True)
                 elif i != 1:
                     embed1.add_field(name=cog_names[i], value=(command_names[i])[:-2], inline=True)
